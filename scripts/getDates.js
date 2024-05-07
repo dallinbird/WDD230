f3de8c3ba44ceaa34b92df5c84d6d28178bebd78
Dallin Bird <bir18013@byui.edu>
2024-05-07 10:22:24 -0600

    djfdjfhdjs

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -4,4 +4,4 @@ document.getElementById("copyright").innerHTML = "&copy; " + currentYear;
 
 //for Last Modified
 const lastMod = document.lastModified;
-document.getElementById("lastMod").innerHTML = "last Modified: " + lastMod;
+document.getElementById("lastMod").innerHTML = "Last Modified: " + lastMod;
