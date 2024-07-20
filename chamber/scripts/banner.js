b349232015f8cc78df9f0dffa8b599ca1daf3378
Dallin Bird <bir18013@byui.edu>
2024-07-20 16:17:19 -0600

    sdsdsds

--- chamber/scripts/banner.js
+++ chamber/scripts/banner.js
@@ -4,7 +4,7 @@ function showBanner() {
 	const date = new Date();
 	const day = date.getDay();
 	const close = document.querySelector("#closer");
-	if (day === 1 || day === 2 || day === 3 || day === 5 || day === 6){
+	if (day === 1 || day === 2 || day === 3 ){
 		console.log(day);
         banner.classList.remove("hidden");
 		banner.classList.add("show");
